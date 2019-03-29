3dcd4306b8fe2f8bda8e62d98dfd2ff79fce4b4c
DHARMESH PODDAR <dharmeshpoddar3@gmail.com>
2019-03-30 01:32:42 +0530

    initial phase

---
+++ src/components/counter.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+class Counter extends Component {
+   
+     state={
+       Count :0,
+       imageUrl: "https://picsum.photos/2000",
+       tags: ['tag1','tag2','tag3']
+
+
+     };
+     constructor(){
+
+        super();
+        this.handleIncreement=this.handleIncreement.bind(this);
+
+     }
+  
+
+    renderTags(){
+       if(this.state.tags==0) return <p>there are no tags</p>;
+  return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+
+
+    }
+
+    handleIncreement = () => {
+      console.log("aur dharmesh");
+      
+       // this.setState({Count : this.state.Count +1});
+
+    }
+    
+    render() { 
+        return (<div>
+            <span  className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <img src="this.state.imageUrl" alt=""/>
+            <h1>Hello World</h1>
+            <button onClick={this.handleIncreement()} className="btn btn-secondary btn-sm">Increement</button> 
+            
+          
+            
+            </div>);
+
+    }
+
+    formatCount(){
+
+        return (this.state.Count===0?'Zero':this.state.Count);
+
+
+    }
+
+    getBadgeClasses(){
+    
+         let classes="badge m-2 badge-";
+         classes +=this.state.Count==0? "warning":"primary";
+         return classes;
+     
+
+    }
+}
+ 
+export default Counter;
